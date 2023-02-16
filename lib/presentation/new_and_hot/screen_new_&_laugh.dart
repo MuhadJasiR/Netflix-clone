8c601f0321ae2fd6e53c0e6f209ca03ede23ae1c
MuhadJasiR <alijasir051@gmail.com>
2023-02-16 16:11:38 +0530

    Home page ui completed

--- lib/presentation/new_and_hot/screen_new_&_laugh.dart
+++ lib/presentation/new_and_hot/screen_new_&_laugh.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../../core/constraints/constants.dart';
+import '../widgets/app_bar_widget.dart';
+
 class ScreenNewAndHot extends StatelessWidget {
   const ScreenNewAndHot({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: AppBar(
+          title: Text(
+            "New & Hot",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.cast,
+                  color: Colors.white,
+                  size: 30,
+                )),
+            Container(
+              height: 30,
+              width: 30,
+              color: Colors.blue,
+            ),
+            kWidth
+          ],
+        ),
+      ),
       body: Text("ScreenNewAndHot"),
     );
   }
